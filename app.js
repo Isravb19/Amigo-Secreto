1ce6034385483894a9cdc536717f050103bbcd53
Israel <israelvb19@gmail.com>
2025-03-17 14:22:34 -0600

    actualizacion js

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// creacion del array para almacenar la lista de amigos y declaración de variables//
+
+const listaAmigos = [];
+
+// se crea una función para agregar amigos, lanzando una alerta si el campo está vacío//
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, ingresa un nombre antes de añadir.");
+        return;
+    }
+
+
+    listaAmigos.push(nombre);
+    actualizarLista();
+    input.value = "";
+}
+
+// Se crea la función para actualizar la lista de amigos y que muestra la lista de amigos en pantalla//
+function actualizarLista() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    listaAmigos.forEach((amigo, index) => {
+        const li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+// función para elegir aleatriamente al amigo secreto//
+
+function sortearAmigo() {
+    if (listaAmigos.length === 0) {
+        alert("La lista está vacía. Agrega al menos un amigo para sortear.");
+        return;
+    }
+
+    const indiceSorteado = Math.floor(Math.random() * listaAmigos.length);
+    const amigoSorteado = listaAmigos[indiceSorteado];
+
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>El amigo secreto es: <strong>${amigoSorteado}</strong></li>`;
+}
